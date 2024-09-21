6eb5835ee690c377b0dab4f810848449d4b495f5
KSteinke <kacpersteinkesb@gmail.com>
2024-09-21 16:13:53 +0200

    bug fix

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -2,7 +2,8 @@
 const menuItems = document.querySelectorAll(".menu-item");
 menuItems.forEach((item, id) => {
     item.addEventListener("click", ()=> {
-        let name =  document.querySelector('.menu-item-name').textContent;
+        
+        let name =  item.querySelector(".menu-item-name");
         let image = document.getElementById(name);
 
         image.style.right = 0;
