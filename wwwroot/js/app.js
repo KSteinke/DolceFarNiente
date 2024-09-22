a9ee4b89a70302f334731359d3677334278047bb
KSteinke <kacpersteinkesb@gmail.com>
2024-09-22 13:41:37 +0200

    minor updates and clean

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -2,7 +2,7 @@
 const menuItems = document.querySelectorAll(".menu-item");
 menuItems.forEach((item, id) => {
     item.addEventListener("click", ()=> {
-        
+        //TODO - Check if there will be no error when 2 were clicked add fixing code if neede
         let name =  item.querySelector(".menu-item-name").textContent;
         let image = document.getElementById(name);
 
